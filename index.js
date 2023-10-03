6efaee23027e75a47fcc6454de871418da22f2b1
bfsylvain <sylvainbonnaure@hotmail.com>
2023-10-03 10:32:56 +0200

    node wilders exercise

---
+++ index.js
@@ -0,0 +1,14 @@
+import chalk from "chalk";
+
+console.log(
+    chalk.green("Benoit")
+)
+console.log(
+    chalk.red("Antoine")
+)
+console.log(
+    chalk.white("Victor")
+)
+console.log(
+    chalk.magenta("Nélia")
+)
